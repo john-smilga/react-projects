18600bde6d46aa49fefaa2e12e99f3fc6818215e
john-smilga <johnsmilga@gmail.com>
2020-10-04 11:54:45 -0700

    first project complete

--- 1-birthday-reminder/final/src/App.js
+++ 1-birthday-reminder/final/src/App.js
@@ -3,7 +3,6 @@ import data from './data';
 import List from './List';
 function App() {
   const [people, setPeople] = useState(data);
-
   return (
     <main>
       <section className='container'>
