d39f13102162c0875fc1a79411a45154c42e3439
john-smilga <johnsmilga@gmail.com>
2020-10-07 18:59:10 -0700

    5-Menu Project Complete

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,21 +4,7 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
-
-  return (
-    <main>
-      <section className='menu section'>
-        <div className='title'>
-          <h2>our menu</h2>
-          <div className='underline'></div>
-        </div>
-        <Categories />
-        <Menu items={menuItems} />
-      </section>
-    </main>
-  );
+  return <h2>Menu Project Setup</h2>;
 }
 
 export default App;
--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
-const Menu = ({ items }) => {
-  return (
-    <div className='section-center'>
-      {items.map((menuItem) => {
-        const { id, title, img, desc, price } = menuItem;
-        return (
-          <article key={id} className='menu-item'>
-            <img src={img} alt={title} className='photo' />
-            <div className='item-info'>
-              <header>
-                <h4>{title}</h4>
-                <h4 className='price'>${price}</h4>
-              </header>
-              <p className='item-text'>{desc}</p>
-            </div>
-          </article>
-        );
-      })}
-    </div>
-  );
+const Menu = () => {
+  return <h2>Menu Component</h2>;
 };
 
 export default Menu;
