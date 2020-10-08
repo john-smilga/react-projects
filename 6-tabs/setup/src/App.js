63aa61ce10f8c606ca86f892370f82c0b4a2135a
john-smilga <johnsmilga@gmail.com>
2020-10-08 09:48:00 -0700

    6-tabs recording complete

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
 function App() {
-  return <h2>Tabs Project Setup</h2>;
+  return <h2>tabs project setup</h2>;
 }
 
 export default App;
