7692f8db2fd4ff37d60d1991e69d613c2d36c285
john-smilga <johnsmilga@gmail.com>
2020-10-09 09:17:37 -0700

    8-lorem-ipsum after recording

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   
+  
   return (
-    <h2>lorem ipsum project setup</h2>
-  );
+  <h2>lorem ipsum project setup</h2>
+    )
 }
 
 export default App;
