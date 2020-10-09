725f667c22f77087d13c025f9e98a285169d82bc
john-smilga <johnsmilga@gmail.com>
2020-10-09 08:33:30 -0700

    8-lorem-ipsum added pre recording

---
+++ 8-lorem-ipsum/final/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
---
+++ 8-lorem-ipsum/setup/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  
+  return (
+    <h2>lorem ipsum project setup</h2>
+  );
+}
+
+export default App;
