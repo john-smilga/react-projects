5d408c8341409df47ed780d628f1984f87c42448
jabinx92 <52295380+jabinx92@users.noreply.github.com>
2020-10-27 15:00:16 -0700

    Update data.js
    spelling error

--- 4-accordion/final/src/data.js
+++ 4-accordion/final/src/data.js
@@ -1,7 +1,7 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookes?',
+    title: 'Do I have to allow the use of cookies?',
     info:
       'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
   },
