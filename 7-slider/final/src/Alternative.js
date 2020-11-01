7a2d854f92e17163811d1cbe2706be64a685e78b
John <johnsmilga@gmail.com>
2020-11-01 11:29:48 -0800

    7-slider alternative setup

--- 7-slider/final/src/Alternative.js
+++ 7-slider/final/src/Alternative.js
@@ -6,15 +6,24 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = React.useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
 
   useEffect(() => {
     let slider = setInterval(() => {
@@ -57,10 +66,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
--- 7-slider/final/src/index.js
+++ 7-slider/final/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Alternative from './Alternative';
 
 ReactDOM.render(
   <React.StrictMode>
