690bed6184e65c60f8d2a1efd42159417ab00551
john-smilga <johnsmilga@gmail.com>
2020-11-02 09:25:21 -0800

    7-slider alternative autoslide fixed

--- 7-slider/final/src/Alternative.js
+++ 7-slider/final/src/Alternative.js
@@ -1,80 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
 function App() {
-  const [people, setPeople] = useState(data);
-  const [index, setIndex] = React.useState(0);
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = React.useState(0)
 
   const nextSlide = () => {
     setIndex((oldIndex) => {
-      let index = oldIndex + 1;
+      let index = oldIndex + 1
       if (index > people.length - 1) {
-        index = 0;
+        index = 0
       }
-      return index;
-    });
-  };
+      return index
+    })
+  }
   const prevSlide = () => {
     setIndex((oldIndex) => {
-      let index = oldIndex - 1;
+      let index = oldIndex - 1
       if (index < 0) {
-        index = people.length - 1;
+        index = people.length - 1
       }
-      return index;
-    });
-  };
+      return index
+    })
+  }
+
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1
+  //   if (index < 0) {
+  //     setIndex(lastIndex)
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0)
+  //   }
+  // }, [index, people])
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
-    }, 5000);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1
+        if (index > people.length - 1) {
+          index = 0
+        }
+        return index
+      })
+    }, 5000)
     return () => {
-      clearInterval(slider);
-    };
-  }, [index]);
+      clearInterval(slider)
+    }
+  }, [index])
 
   return (
-    <section className="section">
-      <div className="title">
+    <section className='section'>
+      <div className='title'>
         <h2>
           <span>/</span>reviews
         </h2>
       </div>
-      <div className="section-center">
+      <div className='section-center'>
         {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person;
+          const { id, image, name, title, quote } = person
 
-          let position = 'nextSlide';
+          let position = 'nextSlide'
           if (personIndex === index) {
-            position = 'activeSlide';
+            position = 'activeSlide'
           }
           if (
             personIndex === index - 1 ||
             (index === 0 && personIndex === people.length - 1)
           ) {
-            position = 'lastSlide';
+            position = 'lastSlide'
           }
 
           return (
             <article className={position} key={id}>
-              <img src={image} alt={name} className="person-img" />
+              <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
             </article>
-          );
+          )
         })}
-        <button className="prev" onClick={prevSlide}>
+        <button className='prev' onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={nextSlide}>
+        <button className='next' onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
     </section>
-  );
+  )
 }
 
-export default App;
+export default App
