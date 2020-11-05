1960d286e75893b1ae7f0f26dcbd14cef2b2ea4b
john-smilga <johnsmilga@gmail.com>
2020-11-05 08:57:53 -0800

    19-stock-photos some changes in functionality added during recording

--- 19-stock-photos/final/src/App.js
+++ 19-stock-photos/final/src/App.js
@@ -9,7 +9,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
   const [query, setQuery] = useState('')
   const fetchImages = async () => {
     setLoading(true)
@@ -60,8 +60,7 @@ function App() {
   }, [])
   const handleSubmit = (e) => {
     e.preventDefault()
-    setPage(() => 1)
-    fetchImages()
+    setPage(1)
   }
   return (
     <main>
