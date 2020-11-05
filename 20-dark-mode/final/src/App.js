7e804b4e436167f3ef66f5fc2c1f35599bfd9dec
John <johnsmilga@gmail.com>
2020-11-05 11:39:51 -0800

    20-dark-mode local storage added

--- 20-dark-mode/final/src/App.js
+++ 20-dark-mode/final/src/App.js
@@ -1,38 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
 
 function App() {
-  const [theme, setTheme] = useState('light-theme')
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (theme === 'light-theme') {
-      setTheme('dark-theme')
+      setTheme('dark-theme');
     } else {
-      setTheme('light-theme')
+      setTheme('light-theme');
     }
-  }
+  };
 
   useEffect(() => {
-    document.documentElement.className = theme
-  }, [theme])
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
     <main>
       <nav>
-        <div className='nav-center'>
+        <div className="nav-center">
           <h1>overreacted</h1>
-          <button className='btn' onClick={toggleTheme}>
+          <button className="btn" onClick={toggleTheme}>
             toggle
           </button>
         </div>
       </nav>
-      <section className='articles'>
+      <section className="articles">
         {data.map((item) => {
-          return <Article key={item.id} {...item} />
+          return <Article key={item.id} {...item} />;
         })}
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
