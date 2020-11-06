ff8d85ac30bbecdb0dc5fd69befcccc2b0bc61b5
John <johnsmilga@gmail.com>
2020-11-06 09:35:27 -0800

    21-movie-db update

--- 21-movie-db/final/src/SearchForm.js
+++ 21-movie-db/final/src/SearchForm.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 const SearchForm = () => {
-  const { query, setQuery, isError } = useGlobalContext()
+  const { query, setQuery, error } = useGlobalContext();
   return (
-    <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>search movies</h2>
       <input
-        type='text'
-        className='form-input'
+        type="text"
+        className="form-input"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      {isError && <div className='error'>movie not found !</div>}
+      {error.show && <div className="error">{error.msg}</div>}
     </form>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
--- 21-movie-db/final/src/context.js
+++ 21-movie-db/final/src/context.js
@@ -1,47 +1,45 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
 
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY_2}`;
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [isError, setIsError] = useState(false)
-  const [movies, setMovies] = useState([])
-  const [query, setQuery] = useState('batman')
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: '' });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('batman');
   const fetchMovies = async (url) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      const response = await fetch(url)
-      const data = await response.json()
+      const response = await fetch(url);
+      const data = await response.json();
 
       if (data.Response === 'True') {
-        setMovies(data.Search)
-        setIsError(false)
+        setMovies(data.Search);
+        setError(false);
       } else {
-        setIsError(true)
+        setError({ show: true, msg: data.Error });
       }
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
-  }, [query])
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
 
   return (
-    <AppContext.Provider
-      value={{ isLoading, isError, movies, query, setQuery }}
-    >
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
