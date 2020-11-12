d1dafbb5ad071fb8d08e7e021dc7b208f20f942f
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-11-12 10:55:13 +0000

    Replaced columns useState with dynamic css class change with respect to column size

--- 13-stripe-submenus/final/src/Submenu.js
+++ 13-stripe-submenus/final/src/Submenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
@@ -8,42 +8,29 @@ const Submenu = () => {
     location,
   } = useGlobalContext()
   const container = useRef(null)
-  const [columns, setColumns] = useState('col-2')
   useEffect(() => {
-    setColumns('col-2')
     const submenu = container.current
     const { center, bottom } = location
     submenu.style.left = `${center}px`
     submenu.style.top = `${bottom}px`
     console.log(links)
-    if (links.length === 3) {
-      setColumns('col-3')
-    }
-    if (links.length > 3) {
-      setColumns('col-4')
-    }
   }, [page, location, links])
   return (
-    <aside
-      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
-      ref={container}
-    >
-      <section>
-        <h4>{page}</h4>
-        <div className={`submenu-center ${columns}`}>
-          {links.map((link, index) => {
-            const { url, icon, label } = link
-            return (
-              <a key={index} href={url}>
-                {icon}
-                {label}
-              </a>
-            )
+    <aside ref={container} className={`${isSubMenu ? "submenu show" : "submenu"}`}>
+      <h4>{page}</h4>
+      <div className={`${links.length > 4} ? submenu-center col-4 : submenu-center col-${links.length}`}>
+          {links.map((link,index) => {
+              const {url,icon,label} = link
+              return(
+                  <a key={index} href={url}>
+                      {icon}
+                      {label}
+                  </a>
+              )
           })}
-        </div>
-      </section>
+      </div>
     </aside>
-  )
+  );
 }
 
 export default Submenu
