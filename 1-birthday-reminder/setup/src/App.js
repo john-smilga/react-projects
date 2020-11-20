92f1eedbd075b814fd9d5d2fa23b99df57336f16
samuray90 <ionutsdaniel53@gmail.com>
2020-11-20 11:45:44 +0000

    onClick set to ([]) so it clears the list above

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <main>
-    <section className="container">
-      <h3>0 birthdays today</h3>
-      <List />
-      <button onClick={()=> console.log('ai dat click')}>
-        clear all
-      </button>
-    </section>
-  </main>;
+  const [people,setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={()=> setPeople([])}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
