123c266e095949daaf777a497ef17f1b9104b03a
samuray90 <ionutsdaniel53@gmail.com>
2020-11-20 11:25:36 +0000

    commit

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className="container">
+      <h3>0 birthdays today</h3>
+      <List />
+      <button onClick={()=> console.log('ai dat click')}>
+        clear all
+      </button>
+    </section>
+  </main>;
 }
 
 export default App;
