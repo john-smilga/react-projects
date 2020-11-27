ba26ec2ae91adbdccf83873e3fa08b87bf9055ee
samuray90 <ionutsdaniel53@gmail.com>
2020-11-27 11:23:28 +0000

    ok to go

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading,setLoading] = useState(true);
