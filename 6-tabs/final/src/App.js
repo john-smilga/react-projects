69d67a2854782dca9165961ae28ded2fb38f6340
John <johnsmilga@gmail.com>
2021-02-04 06:13:34 -0800

    fixed typo in tabs project

--- 6-tabs/final/src/App.js
+++ 6-tabs/final/src/App.js
@@ -19,21 +19,21 @@ function App() {
   }, [])
   if (loading) {
     return (
-      <section className='section loading'>
+      <section className="section loading">
         <h1>Loading...</h1>
       </section>
     )
   }
   const { company, dates, duties, title } = jobs[value]
   return (
-    <section className='section'>
-      <div className='title'>
-        <h2>expierence</h2>
-        <div className='underline'></div>
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
       </div>
-      <div className='jobs-center'>
+      <div className="jobs-center">
         {/* btn container */}
-        <div className='btn-container'>
+        <div className="btn-container">
           {jobs.map((item, index) => {
             return (
               <button
@@ -47,21 +47,21 @@ function App() {
           })}
         </div>
         {/* job info */}
-        <article className='job-info'>
+        <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
-          <p className='job-date'>{dates}</p>
+          <p className="job-date">{dates}</p>
           {duties.map((duty, index) => {
             return (
-              <div key={index} className='job-desc'>
-                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
                 <p>{duty}</p>
               </div>
             )
           })}
         </article>
       </div>
-      <button type='button' className='btn'>
+      <button type="button" className="btn">
         more info
       </button>
     </section>
