27751eb775fe8632e7f546b3fdf3abce02abfb4a
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-03 18:39:58 -0400

    tests importing from react-icons library & applying styling

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Review from './Review';
+import {FaGithubSquare} from 'react-icons/fa';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <h2>
+      reviews project setup
+      <FaGithubSquare className="icon"/>  
+    </h2>;
 }
 
 export default App;
