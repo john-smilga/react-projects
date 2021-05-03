6d45b6680ac354de9ed056f4a86bf0186fb5c920
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-02 20:10:18 -0400

    builds list component to display people data on app

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  // const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data);
   return <main>
     <section className="container">
-      <h3>0 birthdays today</h3>
-      <List/>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
       <button onClick={() => console.log("You clicked me.")}>Clear All</button>
     </section>
   </main>
