6a5a90f9b914df92125e2ab7677c04223a987cc7
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-02 20:00:48 -0400

    places static visual build for application

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  // const [people, setPeople] = useState(data);
+  return <main>
+    <section className="container">
+      <h3>0 birthdays today</h3>
+      <List/>
+      <button onClick={() => console.log("You clicked me.")}>Clear All</button>
+    </section>
+  </main>
 }
 
 export default App;
