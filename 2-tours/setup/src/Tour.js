7e60f959fa4d402e54cb20fb2959c2a6bdb81d5d
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-03 12:47:18 -0400

    adds toggle functionality for length of tour info description text

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
+  // by default, set readMore to false, so that we change it to true onClick to reveal full info
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <section className="single-tour">
       <img src={image} alt={name}/>
@@ -9,7 +12,17 @@ const Tour = ({id, image, info, price, name}) => {
           <h4>{name}</h4>
           <h4 className="tour-price">{price}</h4>
         </div>
-        <p>{info}</p>
+        {/* <p>{info}</p> */}
+        {/* to use "Read More", we need to set up conditional rendering on {info} */}
+        {/* ternary is useful for this */}
+        <p>
+          {readMore ? info : `${info.substring(0,200)} . . .`}
+          <button onClick={()=> setReadMore(!readMore)}>
+          {readMore?"show less":"read more"}
+        </button>
+        
+        </p>
+        
         <button className="delete-btn">Not Interested</button>
       </footer>
     </section>
