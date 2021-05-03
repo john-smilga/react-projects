63d14ac7b792666d6b0b131d83e0e47398ef7dd4
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-02 21:36:00 -0400

    begins project, adds async call to fetch data for tours and places on state

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,7 +5,40 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  };
+
+  useEffect(()=> {
+    fetchTours();
+  }, []);
+
+  if(loading) {
+    return (
+      <main>
+        <Loading/>
+      </main>
+    );
+  }
+  return (
+  <main>
+    <Tours/>
+  </main>
+  )
 }
 
 export default App
