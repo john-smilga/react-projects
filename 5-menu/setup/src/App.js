d2a8f1702fe3f829f3531d24e230fbcc5258fbd7
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-04 19:18:15 -0400

    finishes menu buttons and categories filter

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,10 +3,20 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+// in our filter, we get unique values by using Set instead of having repeated identical entries for our categories
+// then, we use the spread operator after also including "All" at the beginning of the array to include a non-filtered category
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);    
   };
@@ -18,8 +28,8 @@ function App() {
         <div className="underline"></div>
       </div>
 
-      <Categories filterItems={filterItems} />
-      <Menu items={menuItems} />  
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
     </section>
   )
 }
--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Categories = ({filteredItems}) => {
-  return <h2>categories component</h2>;
-};
+// {/* <button className="filter-btn" onClick={()=> filteredItems('all')}>all</button>
+// <button className="filter-btn" onClick={()=> filteredItems('breakfast')}>breakfast</button>
+// <button className="filter-btn" onClick={()=> filteredItems('lunch')}>lunch</button>
+// <button className="filter-btn" onClick={()=> filteredItems('dinner')}>dinner</button> 
+// manual entry isn't DRY; so, this isn't our best approach, especially for future menu categories
+// */}
+
+const Categories = ({categories, filterItems}) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button type="button" className="filter-btn" key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
 
 export default Categories;
