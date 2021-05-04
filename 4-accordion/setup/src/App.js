0d61e69e7e5656a2887232cc2d0bb30f79ea9ef2
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-03 22:03:23 -0400

    begins accordion project; boilerplates by displaying App, Question component, and logging data to console

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  console.log(data);
+  const {id, title, info} = data; 
+
+  return(
+    <article className="container">  
+      <h2>Questions And Answers About Login</h2>
+      <SingleQuestion data={data} />
+    </article>
+  )
 }
 
 export default App;
