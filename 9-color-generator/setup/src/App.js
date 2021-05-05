af6951258606aad8d8614da963e68fb4ebafc7b7
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-05 17:10:04 -0400

    boilerplates for adding in array and API next

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -2,9 +2,31 @@ import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
+  
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("Hello");
+  }
+  return (
+    <div>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" value={color} placeholder="#ff195e" onChange={(e) => setColor(e.target.value)}/>
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        The list of colors goes here:
+      </section>
+    </div>
+  )
 }
 
 export default App
