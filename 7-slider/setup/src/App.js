9a5783aeb6dfceceecc923c6b18c619e950068fc
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-04 22:20:42 -0400

    implements carousel/slider, using native css animations, opacity, & positioning with classes

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -1,9 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
+import { CgQuoteO } from 'react-icons/cg';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0);
+
+  useEffect(()=> {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex)
+    } else if (index > lastIndex) {
+      setIndex(0);
+    }
+  })
+
+  useEffect(() => {
+  let slider = setInterval(()=> {
+      setIndex(index + 1);
+    }, 4000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+        reviews <span><CgQuoteO/></span>
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex)=> {
+          const {id, image, name, title, quote} = person;
+          // more stuff coming up in this part
+          
+          let position = "nextSlide";
+
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
+            position = "lastSlide";
+          }
+          
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img"/>
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"/>
+            </article>
+          )
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft/>
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight/>
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default App;
