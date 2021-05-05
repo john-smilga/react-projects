a6ca65d83d5612422114ed930319dc346cdaccb5
m4rkh0ng <m4rkh0ng@gmail.com>
2021-05-05 17:51:18 -0400

    finishes base version of App component <need to work on SingleColor now

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -11,19 +11,33 @@ function App() {
 
   const handleSubmit = (e) =>{
     e.preventDefault();
-    console.log("Hello");
+    // let colors = new Values(color).all(10);
+    // console.log(colors);
+    try {
+      let colors = new Values(color).all(10);
+      // console.log(colors);
+      setList(colors);
+      setError(false);
+    } catch {
+      setError(true);
+      console.log("Whoops! Can't do that.");
+    }
   }
   return (
     <div>
       <section className="container">
         <h3>color generator</h3>
         <form action="" onSubmit={handleSubmit}>
-          <input type="text" value={color} placeholder="#ff195e" onChange={(e) => setColor(e.target.value)}/>
+          <input className={`${error ? "error" : null}`} type="text" value={color} placeholder="#ff195e" onChange={(e) => setColor(e.target.value)}/>
           <button className="btn" type="submit">Submit</button>
+          <span className={`${error ? "error" : null}`}>{error ? " Valid hex codes only!" : "" }</span>
         </form>
       </section>
       <section className="colors">
-        The list of colors goes here:
+        {list.map((color, index)=>{
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />
+        })}
       </section>
     </div>
   )
