5c50c30d8aabcf218e4de440b3b15142d314a04e
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-05-08 16:37:52 +0100

    last update

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, SetPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}></List>
+        <button onClick={() => SetPeople([])}>delete items</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading></Loading>
+      </main>
+    );
+  }
+
+  return <main>
+    <Tours>
+    </Tours>
+  </main>;
 }
 
-export default App
+export default App;
