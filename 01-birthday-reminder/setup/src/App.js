136bbee202017b068a432cbda456091f12b9f92d
Yash Sharma <55599815+YashSharma373@users.noreply.github.com>
2021-05-19 23:58:00 +0530

    initial commit

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,42 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+import './App.css';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [persons,setPerson] =  useState(data);
+  function handleClick(){
+    setPerson([]);
+  }
+
+  function delteName(key){
+  const arr = persons.filter((person)=>{
+    return(
+        person.id!==key
+    )
+  })
+
+    setPerson(arr);
+  }
+
+  return (
+    <div className="container">
+      <div className="card">
+      <p className="list-length">{persons.length} birthday's today</p>
+      {persons.map((person,i)=>{
+        const name=person.name;
+        const age = person.age;
+        const image = person.image;
+        const id = person.id;
+        return (
+              <List key={i+1} id={id} name={name} age={age} image={image} />
+        )
+      })}
+      <button onClick={handleClick}>Clear All</button>
+      </div>
+    </div>
+      
+  )
 }
 
 export default App;
