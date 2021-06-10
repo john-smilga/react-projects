a67bd5e456d03d343c15322eaac031e15338f197
Abhisek <abhisek.khuntia16@gmail.com>
2021-06-11 00:43:03 +0530

    AppProvider not Included
    AppProvider was not Included

--- 12-sidebar-modal/final/src/App.js
+++ 12-sidebar-modal/final/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Modal from './Modal';
 import Sidebar from './Sidebar';
 import Home from './Home';
+import {AppProvider} from './context'
+
 function App() {
   return (
-    <>
+      <AppProvider>
       <Home />
       <Modal />
       <Sidebar />
-    </>
+      </AppProvider>
   );
 }
 
