4bc5ceb91d619b02399494086f007ba09724e724
Yogesh Kumar <ykapr17@gmail.com>
2021-07-24 11:14:27 +0530

    Update App.js

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h2>reminder project setu</h2>;
+
 }
 
 export default App;
