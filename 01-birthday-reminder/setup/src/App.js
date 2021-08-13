34d5283374323392ff949f6c16e228ad4f474861
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-08-13 17:38:28 +0200

    Finish Project 1

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays Today</h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
