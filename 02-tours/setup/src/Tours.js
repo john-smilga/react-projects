956a156c73cf274545690ecdeb6e05935407895b
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-16 04:20:10 +0100

    tours js

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ Tours, removeTour }) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>our Tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {Tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
