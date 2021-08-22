fff59c6d48f916ed2a52f55ca78a34055dd8383c
Holstein30 <holstein30@live.com>
2021-08-22 17:07:48 -0400

    changed url to gain access to tours object

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,10 +3,22 @@ import Loading from "./Loading";
 import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = "https://course-api.com/react-tours-project";
+const url = "http://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
