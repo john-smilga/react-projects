4122b29e7d8b688115c4e79aac373209fc30352c
Holstein30 <holstein30@live.com>
2021-08-21 20:38:56 -0400

    initial setup - npm install & app.js base

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+      </section>
+    </main>
+  );
 }
 
 export default App;
