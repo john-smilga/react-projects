8a94d1283a984c5eef06fbefd666bfd4bc12fc6c
Holstein30 <holstein30@live.com>
2021-08-22 16:55:14 -0400

    Initial setup

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  return <h2>Tours Project Setup</h2>;
 }
 
-export default App
+export default App;
