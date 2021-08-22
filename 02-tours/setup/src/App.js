c1c85771cc7d98420e30190590c541ce041b3a66
Holstein30 <holstein30@live.com>
2021-08-22 17:35:22 -0400

    project complete - refreshing tours functionality working

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -38,6 +38,18 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
