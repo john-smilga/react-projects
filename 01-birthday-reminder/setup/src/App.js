21351e1b062c0833673d528413c2001e7d04fa95
Holstein30 <holstein30@live.com>
2021-08-21 20:48:06 -0400

    List fully displaying

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => console.log("you clicked me")}>clear all</button>
       </section>
     </main>
   );
