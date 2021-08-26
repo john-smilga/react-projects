7010c4bc09a00f43701d82a75cf17536f68d55bc
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-08-26 13:11:15 +0200

    Finish Project 4

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions & Answers About Login</h3>
+        <section>
+          {
+            questions.map((question) => {
+              return <SingleQuestion {...question} key={question.id} />;
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShow(!show)}>
+          {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {show && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
