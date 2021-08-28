4db29a191e7beb1e58ee54658e550a6552d0c5d4
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-08-28 13:42:01 +0200

    fetch jobs.

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -2,9 +2,43 @@ import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
+
+
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [index, setIndex] = useState(0);
+
+
+  const getJobs = async () => {
+    try {
+
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setJobs(newJobs);
+      setLoading(false);
+
+    } catch (error) {
+      console.log('error :>> ', error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getJobs();
+  }, []);
+
+  if (loading) {
+    return <section className="section loading"><h1>Loading ...</h1></section>;
+  }
+
+  return (
+    <main>
+      jobs
+    </main>
+  );
 }
 
 export default App
