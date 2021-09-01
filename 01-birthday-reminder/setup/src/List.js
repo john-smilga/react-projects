c543901f3c6f4a5764e8501e1c4834ae36d392d4
Can Kanbur <cankanbur1@gmail.com>
2021-09-01 14:47:46 +0300

    .

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ people, removePerson }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map(person => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+              <button onClick={() => removePerson(id)}>Sil</button>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
