cf8330624989c6b3559dca9fb4e28b364c95bc08
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-01-16 14:02:33 +0100

    tours displayed

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours }) => {
+  return (
+    <div className='title'>
+      <h2>Our tours</h2>
+      <div className='underline'></div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Tours;
