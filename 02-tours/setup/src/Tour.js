38b5f818c77125caf47daef625b47e9c77528691
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-01-16 14:12:12 +0100

    read more button

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +10,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">€{price}</h4>
         </div>
-        <p>{info}</p>
+        {/* show the 200 first characters first */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
         <button className="delete-btn">Not interested</button>
       </footer>
     </article>
