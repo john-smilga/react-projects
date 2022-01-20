52f1b506b48998b1ec03745550658b0c7dae6a48
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-01-20 21:24:32 +0100

    completed accordion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            /// ... to load the rest of the question object
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+
+        {/* Whatever is the value of showInfo, set the opposite: */}
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* If showInfo is true, then show my paragraph. It's false by default */}
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
