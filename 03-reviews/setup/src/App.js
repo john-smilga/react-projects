c2899eaf09003be3cc766cc890690f27d3a46313
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-01-20 20:39:54 +0100

    reviews finished

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Review from './Review';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main> 
+    <div className='container'>
+      <div className='title'>
+        <h2>Our reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </div>
+    </main>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,69 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(1);
+  // as the index value changes, so will be the person changing in the array
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    // to have a number between 0 and 3
+    let randomNumber = Math.floor(Math.random() * people.length);
+   // to avoid repetition
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    console.log(randomNumber);
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+        <button className="random-btn" onClick={randomPerson}>
+          Surprise me
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
