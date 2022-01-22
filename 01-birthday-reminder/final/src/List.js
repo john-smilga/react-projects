cc98063a92f3c4d58cd4bfea8f9f36f5b989a197
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-01-22 10:08:27 +0100

    writer birthday json file

--- 01-birthday-reminder/final/src/List.js
+++ 01-birthday-reminder/final/src/List.js
@@ -4,13 +4,13 @@ const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        const { id, name, age, image } = person;
+        const { name, birthday } = person;
         return (
-          <article key={id} className='person'>
-            <img src={image} alt={name} />
+          <article key={name} className="person">
+            {/* <img src={image} alt={name} /> */}
             <div>
               <h4>{name}</h4>
-              <p>{age} years</p>
+              <p>{birthday}</p>
             </div>
           </article>
         );
--- 01-birthday-reminder/final/src/WritersAPI.js
+++ 01-birthday-reminder/final/src/WritersAPI.js
@@ -10,16 +10,18 @@ const url = 'https://www.writerswrite.co.za/literary-birthday-calendar/';
 async function scrapeData() {
   try {
     //Fetch HTML of the page we want to scrape
+
     const { data } = await axios.get(url);
-    // Load HTML we fetched in the previous line
+    // Loads all the HTML of the page we fetched in the previous line
     const $ = cheerio.load(data);
+
     //Select all the list of writers
     const listWriters = $('li, p');
     //Store data for all writers
     const writers = [];
     //Use each method to loop through the li we selected
     listWriters.each((idx, el) => {
-      const writer = { name: '', birthday: '' };
+      const writer = { name: this.name, birthday: this.birthday };
       writer.name = $(el).children('a').text();
       writer.birthday = $(el).children('strong').text(); // find a solution here!
 
@@ -44,5 +46,19 @@ async function scrapeData() {
     console.error(err);
   }
 }
+
+//MANIPULATE THE ARRAY
+
+//What do I want to do?
+
+// #First remove clutter manually: everything before and after the list => done
+// #Then Divide the array in one array per date
+//      ## if name is empty, "cut" the array. (how? slice -> returns subarray.  method match? filter?)
+let array = './writers-birthday.json'
+//  #then give the objects the right birthday date => map? 
+// is it better to have one birthday date per writer? or have an array per date with all the writers?
+// ## Change the string "Writer born 1 January" to a date format. First split(?) the string "Writer born". 
+// Then change month into number and add "0" and "." (e.g. 01.01) => will it match the date format?
+
 // Invoke the above function
 scrapeData();
