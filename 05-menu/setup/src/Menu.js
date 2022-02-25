5e7600b473328a6a4b97a1cf3244586db8b8f844
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-02-25 20:19:32 +0100

    set content for menu

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
