f2134d9c98e34b125e7df7c909489b3f24df53cb
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-03-03 21:27:32 +0100

    finished project 8

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -16,20 +16,22 @@ function App() {
     setText(data.slice(0, amount));
   };
   return (
-    <section className='section-center'>
-      <h3>tired of boring lorem ipsum?</h3>
-      <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraphs:</label>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
         <input
-          type='number'
-          name='amount'
-          id='amount'
+          type="number"
+          name="amount"
+          id="amount"
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button className='btn'>generate</button>
+        <button type="submit" className="btn">
+          generate
+        </button>
       </form>
-      <article className='lorem-text'>
+      <article className="lorem-text">
         {text.map((item, index) => {
           return <p key={index}>{item}</p>;
         })}
