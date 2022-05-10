c1b7f8e720aea5c0381c6f57d99e497da36bde11
AmrAbdelgalil <93317873+AmrAbdelgalil@users.noreply.github.com>
2022-05-10 09:11:15 +0200

    Building Accordion Project from setup folder

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React from "react";
+import data from "./data";
+import Question from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answer about login</h3>
+        <section className="info">
+          {data.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                title={question.title}
+                info={question.info}
+              />
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = (props) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  function handleAnswer() {
+    setShowAnswer((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{props.title}</h4>
+        <button className="btn" onClick={handleAnswer}>
+          {showAnswer ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{showAnswer ? props.info : null}</p>
+    </article>
+  );
 };
 
 export default Question;
