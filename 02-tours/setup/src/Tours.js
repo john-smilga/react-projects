34c7317816e8d98f8440699997092727fdfa6ead
Lance Liu <lanceliumeng89@gmail.com>
2022-06-27 20:01:11 +1000

    finish tour project

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = (props) => {
+  const { tours, removeTour } = props; //props is stand for tours property from App component(parent component)
+
+  return (
+    <section>
+      <div className="title">
+        <h2> Tours </h2>
+        <div className="underline" />
+      </div>
+      <div>
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
