138cbe9e897751e7c0242d70f8823ea2b281693c
josephchimebuka <chimebujoseph@gmail.com>
2022-07-23 04:14:52 +0100

    new updates

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const List = ({people}) => {
-  const {id, name, age, image}= people;
   // {
   //   people.map((person)=>{
   //     const {id, name, age, image}= person;
@@ -15,7 +14,7 @@ const List = ({people}) => {
     people.map((person)=>{
       const {id, name, age, image}= person;
       return <article key={id} className='person'>
-          <img src={image}/>
+          <img src={image} alt={name}/>
           <div>
           <h4>{name}</h4>
           <p>{age} years</p>
