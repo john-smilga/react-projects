cac3b460d4edd5b9d8053d748f8310ac3a674033
josephchimebuka <chimebujoseph@gmail.com>
2022-07-26 14:58:38 +0100

    Tour app build

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  // 
+  // return <div key={id}>
+
+  // </div>;
+
+ return(
+  <section className='title'>
+  <h2>Our tours</h2>
+ <div className='underline'></div>
+ <div>
+  {
+    tours.map((tour)=>{
+      return <Tour key={tour.id}  {...tour} removeTour={removeTour}/>
+    })
+  }
+ </div>
+</section>
+ );
 };
 
 export default Tours;
