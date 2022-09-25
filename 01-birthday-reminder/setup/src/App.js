72b2664640472a6c79d29d2cc36231254add6140
Vijay Kumar <vijay.vuex@gmail.com>
2022-09-25 23:34:18 +0530

    useState hook

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+
+        <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </>
+  );
 }
 
 export default App;
