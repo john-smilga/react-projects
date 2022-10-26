618bbaa9e2c70c94edbcd22e828f4919094da2e4
Manish <manisksing@gmail.com>
2022-10-27 00:35:59 +0530

    Reminder Setup Push-1

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  const Click = () => {
+    setPeople([]);
+  }
+  
+  return <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
+      <button onClick={Click}>Clear All</button>
+      {/* <button onClick={()=> {removeItem(people)}}>View All</button> */}
+    </section>
+  </main>
 }
 
 export default App;
