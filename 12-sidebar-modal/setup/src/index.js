0c6a031f05c08342179dcc140ce53ae173a37285
john smilga <johnsmilga@gmail.com>
2022-12-09 10:55:07 -0500

    upgrade 12 to React 18

--- 12-sidebar-modal/setup/src/index.js
+++ 12-sidebar-modal/setup/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-ReactDOM.render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AppProvider } from './context';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>
+);
