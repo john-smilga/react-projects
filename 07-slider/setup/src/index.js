974c27547d6b63065a8e65462b701e4d22c10c0f
john smilga <johnsmilga@gmail.com>
2022-12-09 10:17:27 -0500

    upgrade 07 to React 18

--- 07-slider/setup/src/index.js
+++ 07-slider/setup/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Alternative from './Alternative';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
