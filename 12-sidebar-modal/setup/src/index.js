3e70c99f416bb854f8ddc42c892cbbec79926a00
john smilga <johnsmilga@gmail.com>
2023-01-07 19:51:46 -0500

    fixed index.js

--- 12-sidebar-modal/setup/src/index.js
+++ 12-sidebar-modal/setup/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AppProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
