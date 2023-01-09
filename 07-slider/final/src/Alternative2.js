af8796a0b1d132243256c8b42b84b892d856494a
john smilga <johnsmilga@gmail.com>
2023-01-08 21:37:00 -0500

    alternative

---
+++ 07-slider/final/src/Alternative2.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      const result = (oldIndex + 1) % people.length;
+      return result;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      const result = (oldIndex - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        const result = (oldIndex + 1) % people.length;
+        return result;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev' onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
--- 07-slider/final/src/index.js
+++ 07-slider/final/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Alternative from './Alternative';
+import Alternative2 from './Alternative2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
