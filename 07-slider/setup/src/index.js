b7be69a43540b6ae48805eacc6177c3520b12be8
john smilga <johnsmilga@gmail.com>
2023-01-22 15:46:25 -0500

    removed alternative

--- 07-slider/setup/src/index.js
+++ 07-slider/setup/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Alternative from './Alternative';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
