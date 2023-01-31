6574161e31be78f1f4a6171b98f7f002d6fb0130
john smilga <johnsmilga@gmail.com>
2023-01-31 11:55:19 -0500

    fix stripe submenus index.js

--- 13-stripe-submenus/setup/src/index.js
+++ 13-stripe-submenus/setup/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AppProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
