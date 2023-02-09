8c0bea945f8375b2819b54bc80d69a22e287bd48
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-02-09 10:23:23 -0500

    working on fetch API

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,6 +5,25 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
+	const [loading, setLoading] = useState(true);
+	const [tour, setTour] = useState([]);
+
+	if (loading) {
+		return <Loading />;
+	}
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const respose = await fetch(url);
+			const tour = await respose.json();
+			setTour(tour);
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<main>
 			<Tours />
