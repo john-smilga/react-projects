02a9a566d68fafafcd3415c37441e67c7ac06404
Matthew Espinoza <matthewespinoza02@gmail.com>
2023-04-26 20:34:22 +0000

    Completed second project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  function filterTours(id) {
+    const newTour = tours.filter((tours) => tours.id !== id);
+    setTours(newTour);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading == true) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if(tours.length == 0) {
+    return (
+      <main>
+        <div className="title">
+                <h3> no tours left </h3>
+                <button className='btn' onClick={() => fetchTours()}> reset</button>
+              </div>
+        </main>
+    )
+  }
+
+  return (
+    <main>
+      <h2 className="title">
+        Our Tours
+        <div className="underline"> </div>
+      </h2>
+      <Tours tours={tours} filterTours={filterTours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React, { useState } from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, filterTours }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <section>
+      {tours.map((tour) => {
+        const { id, name, image, info, price } = tour;
+        return (
+          <article className="single-tour">
+            <img src={image} alt={name} />
+            <footer>
+              <div className="tour-info">
+                <h3> {name} </h3>
+                <h4 className="tour-price">{price}</h4>
+              </div>
+              <p>
+                {readMore ? info : `${info.substring(0, 200)}...`}
+                <button onClick={() => setReadMore(!readMore)}>
+                  {readMore ? "show less" : "read more"}
+                </button>
+              </p>
+              <button className="delete-btn" onClick={() => filterTours(id)}>
+                not interested
+              </button>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Tours;
