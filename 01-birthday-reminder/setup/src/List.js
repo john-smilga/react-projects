dc088e97a3685b4da552e93f00bf02a160cfd402
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-05-06 15:12:38 -0400

    the birthday reminder project is completed

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import App from "./App";
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+	return (
+		<>
+			{people.map((person) => {
+				const { id, name, age, image } = person;
+				return (
+					<article key={id} className="person">
+						<img src={image} alt="" />
+						<div>
+							<h4>{name}</h4>
+							<p>{age} years</p>
+						</div>
+					</article>
+				);
+			})}
+		</>
+	);
 };
 
 export default List;
