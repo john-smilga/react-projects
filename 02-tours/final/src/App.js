611ad30696a5a995fff3efcc4f607c8662a62eeb
saadwaqas688 <86394839+saadwaqas688@users.noreply.github.com>
2023-06-15 19:41:37 +0500

    Update App.js

--- 02-tours/final/src/App.js
+++ 02-tours/final/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
+// ATTENTION!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 
