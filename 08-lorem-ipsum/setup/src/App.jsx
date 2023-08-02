e57a9098ce98a37558ddb92dfc1effcae5e36e7e
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-08-02 18:49:11 +0100

    started task 8

---
+++ 08-lorem-ipsum/setup/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data);
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="Amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
