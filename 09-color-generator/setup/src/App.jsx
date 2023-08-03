063f4ba5b4b37ecf6334a069dee0ce15309123bf
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-08-03 16:52:22 +0100

    task 9 done

---
+++ 09-color-generator/setup/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      setError(false);
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Colour Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
-
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
-
-export default SingleColor
