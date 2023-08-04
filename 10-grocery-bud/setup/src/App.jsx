a71f9782b45b4e752932b8894ba306776c23e2c8
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-08-04 16:26:25 +0100

    started 10

---
+++ 10-grocery-bud/setup/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  return (
+    <section className="section-center">
+      <form></form>
+    </section>
+  );
+}
+
+export default App;
