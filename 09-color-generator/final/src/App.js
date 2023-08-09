f5e74970981044361602b231e74dd1874a5ab9f5
Nazir <91749601+Nazir-Rizwan@users.noreply.github.com>
2023-08-09 11:26:26 -0700

    remove color state string

--- 09-color-generator/final/src/App.js
+++ 09-color-generator/final/src/App.js
@@ -4,7 +4,7 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  const [color, setColor] = useState('')
+  const [color, setColor] = useState()
   const [error, setError] = useState(false)
   const [list, setList] = useState(new Values('#f15025').all(10))
 
