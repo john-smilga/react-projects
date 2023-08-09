e3afdb1b7ce7538dd8ef201a508bae5b0af28774
Bradley Booth <bradley.booth@hotmail.co.uk>
2023-08-09 19:19:48 +0100

    Added proj 10

--- 10-grocery-bud/setup/src/App.jsx
+++ 10-grocery-bud/setup/src/App.jsx
@@ -9,9 +9,42 @@ function App() {
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
   return (
     <section className="section-center">
-      <form></form>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. Eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List items={list} />
+        <button className="clear-btn">clear items</button>
+      </div>
     </section>
   );
 }
--- 10-grocery-bud/setup/src/List.jsx
+++ 10-grocery-bud/setup/src/List.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
